f9d1df0202c0605bd41c675edbb76a8726a4283a
AdrianFondeur <117770305+AdrianFondeur@users.noreply.github.com>
2022-11-18 14:56:05 -0400

    Add files via upload

---
+++ ProyectoFinal/src/logical/Universitario.java
@@ -0,0 +1,29 @@
+import java.util.Date;
+
+public class Universitario extends Persona{
+
+		public Universitario(String cedula, String nombres, String apellidos, String direccion, String telefono,
+			Date fechaNacimiento, char sexo, char nivelEducativo, String carrera, int añoGraduacion) {
+		super(cedula, nombres, apellidos, direccion, telefono, fechaNacimiento, sexo, nivelEducativo);
+		this.carrera = carrera;
+		this.añoGraduacion = añoGraduacion;
+	}
+		private String carrera;
+		private int añoGraduacion;
+		
+		
+		public String getCarrera() {
+			return carrera;
+		}
+		public void setCarrera(String carrera) {
+			this.carrera = carrera;
+		}
+		public int getAñoGraduacion() {
+			return añoGraduacion;
+		}
+		public void setAñoGraduacion(int añoGraduacion) {
+			this.añoGraduacion = añoGraduacion;
+		}
+
+
+}
