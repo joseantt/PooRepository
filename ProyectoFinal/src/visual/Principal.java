87fc0b06fdac60a7a7502a2674007cc750becf21
adria <adria@LAPTOP-R48B0HMH>
2022-11-25 14:46:52 -0400

    Registrar usuario, by EL PRIETO

--- ProyectoFinal/src/visual/Principal.java
+++ ProyectoFinal/src/visual/Principal.java
@@ -2,6 +2,7 @@ package visual;
 
 import java.awt.BorderLayout;
 
+
 import java.awt.Dimension;
 import java.awt.EventQueue;
 
@@ -17,6 +18,17 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
 
 public class Principal extends JFrame {
 
@@ -36,10 +48,29 @@ public class Principal extends JFrame {
 		});
 	}
 
-	
+
 	public Principal() {
 		if(Control.getInstance().getLoginUser()!= null)
 		{
+			addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowClosing(WindowEvent e) {
+					FileOutputStream empresa2;
+					ObjectOutputStream empresaWrite;
+					try {
+						empresa2 = new  FileOutputStream("empresa.dat");
+						empresaWrite = new ObjectOutputStream(empresa2);
+						empresaWrite.writeObject(Control.getInstance());
+					} catch (FileNotFoundException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+				}
+			});
 		setResizable(false);
 		setTitle("Bolsa Laboral");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -82,6 +113,21 @@ public class Principal extends JFrame {
 		JMenu mnNewMenu_1 = new JMenu("Centro Empleador");
 		menuBar.add(mnNewMenu_1);
 		
+		JMenu mnNewMenu_8 = new JMenu("Usuarios");
+		menuBar.add(mnNewMenu_8);
+		
+		JMenuItem mntmNewMenuItem_8 = new JMenuItem("Registrar Usuario");
+		mntmNewMenuItem_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				regUser rCE = new regUser();
+				rCE.setModal(true);
+				rCE.setLocationRelativeTo(null);
+				rCE.setVisible(true);
+			}
+		});
+		
+		mnNewMenu_8.add(mntmNewMenuItem_8);
+		
 		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Registrar Centro Empleador");
 		mntmNewMenuItem_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -91,6 +137,10 @@ public class Principal extends JFrame {
 				rCE.setVisible(true);
 			}
 		});
+		
+		JMenuItem mntmNewMenuItem_9 = new JMenuItem("Listar usuarios");
+		mnNewMenu_8.add(mntmNewMenuItem_9);
+		
 		mnNewMenu_1.add(mntmNewMenuItem_3);
 		
 		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Listado de Centros Empleadores");
