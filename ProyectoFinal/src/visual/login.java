a5dce2b3c6f6588b17d775e4cf9209eba0630144
Usuario <Usuario@192.168.56.1>
2022-11-28 15:01:47 -0400

    Detalles finales del login

--- ProyectoFinal/src/visual/login.java
+++ ProyectoFinal/src/visual/login.java
@@ -17,6 +17,7 @@ import logical.Control;
 import logical.User;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -27,8 +28,8 @@ import javax.swing.JPasswordField;
 public class login extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField textField_1;
+	private JTextField txtUsuario;
+	private JPasswordField txtContraseña;
  
 	/**
 	 * Launch the application.
@@ -96,51 +97,68 @@ public class login extends JFrame {
 		panel.setLayout(null);
 		
 		JLabel lblUsuario = new JLabel("Usuario:");
-		lblUsuario.setBounds(137, 38, 57, 14);
+		lblUsuario.setBounds(149, 37, 57, 14);
 		panel.add(lblUsuario);
 		
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
-		lblContrasea.setBounds(129, 96, 81, 14);
+		lblContrasea.setBounds(137, 95, 81, 14);
 		panel.add(lblContrasea);
 		
 		
-		textField = new JTextField();
-		textField.setBounds(70, 63, 191, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(82, 62, 191, 20);
+		panel.add(txtUsuario);
+		txtUsuario.setColumns(10);
 		
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String myPass=String.valueOf(textField_1.getPassword());
-				if(Control.getInstance().confirmLogin(textField.getText(),myPass)){
+				String myPass=String.valueOf(txtContraseña.getPassword());
+				if(Control.getInstance().confirmLogin(txtUsuario.getText(),myPass)){
+					JOptionPane.showMessageDialog(null, "Bienvenido señor/a "+txtUsuario.getText(), "Información", JOptionPane.INFORMATION_MESSAGE);
 					Principal frame = new Principal();
 					dispose();
 					frame.setVisible(true);
+				}else {
+				JOptionPane.showMessageDialog(null, "El nombre de usuario o la contraseña son incorrectas, intente de nuevo", "Información", JOptionPane.INFORMATION_MESSAGE);
+				clean();
+
 				};
+				
 				
 			}
 		});
-		
+	
 
-		btnLogin.setBounds(121, 174, 89, 23);
+		btnLogin.setBounds(133, 173, 89, 23);
 		panel.add(btnLogin);
 		
-		textField_1 = new JPasswordField();
-		textField_1.setBounds(70, 124, 191, 22);
-		panel.add(textField_1);
+		txtContraseña = new JPasswordField();
+		txtContraseña.setBounds(82, 123, 191, 22);
+		panel.add(txtContraseña);
 		
-		textField_1.addActionListener(new ActionListener() {
+		txtContraseña.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String myPass=String.valueOf(textField_1.getPassword());
-				if(Control.getInstance().confirmLogin(textField.getText(),myPass)){
+				String myPass=String.valueOf(txtContraseña.getPassword());
+				if(Control.getInstance().confirmLogin(txtUsuario.getText(),myPass)){
+					JOptionPane.showMessageDialog(null, "Bienvenido señor/a "+txtUsuario.getText(), "Información", JOptionPane.INFORMATION_MESSAGE);
 					Principal frame = new Principal();
 					dispose();
 					frame.setVisible(true);
+				}else {
+					JOptionPane.showMessageDialog(null, "El nombre de usuario o la contraseña son incorrectas, intente de nuevo", "Información", JOptionPane.INFORMATION_MESSAGE);
+					clean();
 				};
 			}
 
+			
+
 		});
+	}
+	private void clean() {
+		txtContraseña.setText("");
+		txtUsuario.setText("");
+		txtUsuario.requestFocus();
 	}
 }
